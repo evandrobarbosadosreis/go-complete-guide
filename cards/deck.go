a0b10c95091593f11068db0c0d3a580b146691f6
Evandro Barbosa dos Reis <evandrobarbosadosreis@gmail.com>
2022-02-16 23:03:00 -0300

    feature: add shuffle function

--- cards/deck.go
+++ cards/deck.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -52,4 +54,13 @@ func newDeckFromFile(filename string) deck {
 		os.Exit(1)
 	}
 	return deck(strings.Split(string(data), "|"))
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	myRand := rand.New(source)
+	for index := range d {
+		newPosition := myRand.Intn(len(d) - 1)
+		d[index], d[newPosition] = d[newPosition], d[index]
+	}
 }

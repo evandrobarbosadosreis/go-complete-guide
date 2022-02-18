5aa18dea0036f874642d9788ee7057a64f32a688
Evandro Barbosa dos Reis <evandrobarbosadosreis@gmail.com>
2022-02-17 21:52:05 -0300

    refactor: change fmt call

--- task1/main.go
+++ task1/main.go
@@ -4,13 +4,13 @@ import "fmt"
 
 func main() {
 
-	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
 	for _, number := range numbers {
 		if isEven(number) {
-			fmt.Printf("%d is even\n", number)
+			fmt.Println(number, " is even", number)
 		} else {
-			fmt.Printf("%d is odd\n", number)
+			fmt.Println(number, " is odd", number)
 		}
 	}
 }
